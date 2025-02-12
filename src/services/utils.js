cec643b2ab4c343a6c5096233970ec9e04f4c2c1
Adr <152195399+acsu132@users.noreply.github.com>
2025-02-12 16:08:31 -0300

    Update utils.js

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,13 +1,17 @@
-const axios = require('axios');
-
 exports.getDataFromUrl = async (url) => {
     const html = await axios({
         method: 'get',
         url: `https://www.gsmarena.com${url}`,
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36',
+            'Accept-Language': 'en-US,en;q=0.9',
+            'Referer': 'https://www.google.com/'
+        }
     });
 
     return html.data;
 };
+
 
 exports.getPrice = (text) => {
     const value = text.replace(',', '').split(' ');
