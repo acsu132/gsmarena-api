08d7d11cfd54d00a0eb61e1516d55acb67649df9
Adr <152195399+acsu132@users.noreply.github.com>
2025-02-15 11:07:28 -0300

    Update utils.js

--- src/services/utils.js
+++ src/services/utils.js
@@ -3,9 +3,10 @@ const chromium = require('chromium');
 
 exports.getDataFromUrl = async (url) => {
     const browser = await puppeteer.launch({
-        executablePath: chromium.path, // Usa o Chromium do Render
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+    executablePath: chromium.path, // Usa o Chromium do Render
+    args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    protocolTimeout: 60000 // Aumenta o tempo limite para 60s
+});
 
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
