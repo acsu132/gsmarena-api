9628eb1a68549c34e010696d1514b9f3314c9ea9
Adr <152195399+acsu132@users.noreply.github.com>
2025-02-15 10:10:50 -0300

    Update utils.js

--- src/services/utils.js
+++ src/services/utils.js
@@ -9,6 +9,15 @@ exports.getDataFromUrl = async (url) => {
 
     const page = await browser.newPage();
     await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36');
+    await page.setRequestInterception(true);
+    page.on('request', (req) => {
+    if (['stylesheet', 'font', 'image'].includes(req.resourceType())) {
+        req.abort();
+    } else {
+        req.continue();
+    }
+});
+
     
     await page.goto(`https://www.gsmarena.com${url}`, { waitUntil: 'domcontentloaded' });
     const html = await page.content();
