878caa60f6d88f0d544a612c8841845b68534346
Adr <152195399+acsu132@users.noreply.github.com>
2025-02-15 12:16:55 -0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const search = require('./src/services/search');
 const top = require('./src/services/top');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
     res.json({ message: 'GSMArena API is running' });
